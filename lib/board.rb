a82de10c74215b45791d4abaf3b7e5c1aa64a1bd
Jorge Andrade <jorge.andrade@gnovus.ca>
2022-02-19 01:45:23 -0600

    removeed uunsued method

--- lib/board.rb
+++ lib/board.rb
@@ -13,11 +13,6 @@ module GameOfLife
       end
     end
 
-    def cells_total(size)
-      @cells_total ||= size * size
-    end
-
-
     # Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
     # Any live cell with two or three live neighbours lives on to the next generation.
     # Any live cell with more than three live neighbours dies, as if by overpopulation.
